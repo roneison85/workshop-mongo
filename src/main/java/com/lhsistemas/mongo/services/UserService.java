aeb0a2d693646e813aca7785e2ac4a58a39c9d9e
roneison85 <roneison.machado@gmail.com>
2021-12-23 11:04:24 -0300

    Inserção de usuário com POST

--- src/main/java/com/lhsistemas/mongo/services/UserService.java
+++ src/main/java/com/lhsistemas/mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lhsistemas.mongo.domain.User;
+import com.lhsistemas.mongo.dto.UserDTO;
 import com.lhsistemas.mongo.repositories.UserRepository;
 import com.lhsistemas.mongo.services.exceptions.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDto(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
