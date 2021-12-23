3089a6421e5d16055dcfa5bd37e2b426b166fc3f
roneison85 <roneison.machado@gmail.com>
2021-12-23 16:51:49 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/lhsistemas/mongo/config/Instantiation.java
+++ src/main/java/com/lhsistemas/mongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.lhsistemas.mongo.domain.Post;
 import com.lhsistemas.mongo.domain.User;
 import com.lhsistemas.mongo.dto.AuthorDTO;
+import com.lhsistemas.mongo.dto.CommentDTO;
 import com.lhsistemas.mongo.repositories.PostRepository;
 import com.lhsistemas.mongo.repositories.UserRepository;
 
@@ -37,10 +38,19 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
+		
+		postRepository.deleteAll();
+		
 		Post post1 = new Post(null, format.parse("22/12/2021"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, format.parse("23/12/2021"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
-		postRepository.deleteAll();
+		CommentDTO c1 = new CommentDTO("Boa viagem, mano!", format.parse("22/12/2021"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", format.parse("22/12/2021"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia.", format.parse("22/12/2021"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
