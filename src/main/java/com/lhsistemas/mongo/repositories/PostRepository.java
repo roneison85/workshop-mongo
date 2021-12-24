5b0d771ed5f4bfc423ce5aaefa50044b46a9cee3
roneison85 <roneison.machado@gmail.com>
2021-12-24 10:37:20 -0300

    Consulta simples com @Query

--- src/main/java/com/lhsistemas/mongo/repositories/PostRepository.java
+++ src/main/java/com/lhsistemas/mongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.lhsistemas.mongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lhsistemas.mongo.domain.Post;
@@ -10,5 +11,8 @@ import com.lhsistemas.mongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String title);
+	
 	List<Post> findByTitleContainingIgnoreCase(String title);
 }
